f01b65da70fb4d4e6bbb99db2533ac7370741bdc
ducnguyen <minhducnguyen189@gmail.com>
2024-05-06 22:24:49 +0700

    add example for spliting apps into components

---
+++ udemy/spliting-apps-into-components/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ServerElement } from '../server-element/server-element.model';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.scss']
+})
+export class CockpitComponent {
+
+  newServerName = '';
+  newServerContent = '';
+
+  @Output()
+  serverElement: EventEmitter<ServerElement> = new EventEmitter<ServerElement>();
+
+  onAddServer() {
+   const element: ServerElement = this.buildServerElement('server');
+   this.serverElement.emit(element);
+  }
+
+  onAddBlueprint() {
+    const element: ServerElement = this.buildServerElement('blueprint');
+    this.serverElement.emit(element);
+  }
+
+  private buildServerElement(serverType: string): ServerElement {
+      return {
+        type: serverType,
+        name: this.newServerName,
+        content: this.newServerContent
+     };
+  }
+
+}
